ff5591b2005979f4da9ef8d0a9b0fb9fddb75c7c
daffodilho <46694934+daffodilho@users.noreply.github.com>
2019-05-25 22:24:36 -0400

    made changes to html css js and readme

--- js/main.js
+++ js/main.js
@@ -3,42 +3,86 @@
     
     //elements
     let allIcons = document.querySelectorAll("#iconWrapper div"),
-        //facebook = document.querySelector("#facebook"),
-        allSVG = document.querySelectorAll("#iconWrapper svg");
-        
-        
+        facebook = document.querySelector("#facebook"),
+        googleplus = document.querySelector("#googleplus"),
+        instagram = document.querySelector("#instagram"),
+        linkedin = document.querySelector("#linkedin"),
+        pinterest = document.querySelector("#pinterest"),
+        snapchat = document.querySelector("#snapchat"),
+        tumblr = document.querySelector("#tumblr"),
+        twitter = document.querySelector("#twitter"),
+        whatsapp = document.querySelector("#whatsapp"),
+        youtube = document.querySelector("#youtube");
 
-    
     //function
-    function logMyId() {
+    function logMyId(){
         console.log(this.firstChild.id);
-        //allIcons.classList.toggle("selected");
     }
     
-    function makeATurn() {
-        allSVG.classList.toggle("selected");
+    function facebookTurn(){
+        facebook.classList.toggle("selected");
     }
     
-    //function makeATurn(){
-        //facebook.classList.toggle("selected");
-    //}
+    function googleplusTurn(){
+        googleplus.classList.toggle("selected");
+    }
     
+    function instagramTurn(){
+        instagram.classList.toggle("selected");
+    }
     
+    function linkedinTurn(){
+        linkedin.classList.toggle("selected");
+    }
+    
+    function pinterestTurn(){
+        pinterest.classList.toggle("selected");
+    }
+    
+    function snapchatTurn(){
+        snapchat.classList.toggle("selected");
+    }
+    
+    function tumblrTurn(){
+        tumblr.classList.toggle("selected");
+    }
+    
+    function twitterTurn(){
+        twitter.classList.toggle("selected");
+    }
+    
+    function whatsappTurn(){
+        whatsapp.classList.toggle("selected");
+    }
+    
+    function youtubeTurn(){
+        youtube.classList.toggle("selected");
+    }
     
     //events
     allIcons.forEach(item=>{
         item.addEventListener("click", logMyId);
     })
     
-    allSVG.forEach(item=>{
-        item.addEventListener("click", makeATurn);
-    })
+    facebook.addEventListener("click", facebookTurn);
     
-    //facebook.addEventListener("click", makeATurn);
+    googleplus.addEventListener("click", googleplusTurn);
     
+    instagram.addEventListener("click", instagramTurn);
     
+    linkedin.addEventListener("click", linkedinTurn);
     
+    pinterest.addEventListener("click", pinterestTurn);
     
+    snapchat.addEventListener("click", snapchatTurn);
+    
+    tumblr.addEventListener("click", tumblrTurn);
+    
+    twitter.addEventListener("click", twitterTurn);
+    
+    whatsapp.addEventListener("click", whatsappTurn);
+    
+    youtube.addEventListener("click", youtubeTurn);
     
 })();
     
